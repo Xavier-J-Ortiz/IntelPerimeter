65272a471a83981c1d2ab49de82e89a53214d29b
Xavier Ortiz <xavier.ortiz.ch@gmail.com>
2021-08-19 18:20:56 -0400

    successfully used grequests. Need to look into cleaning up error requests to not hit the error limit

--- build-graph/graphBuilder.py
+++ build-graph/graphBuilder.py
@@ -1,19 +1,35 @@
-import requests
+import grequests
 import json
-first_system = 30000001
-last_system = 30000009
-#last_system = 30045354
+from requests.exceptions import HTTPError
 
+first_system = 30000001
+last_system = 30000326
+system_segments=[]
+amount_of_systems = last_system - first_system + 1
+request_headers = {
+    "accept": "application/json",
+    "Accept-Language": "en",
+    "Cache-Control": "no-cache"
+}
+
+f= open("test.txt","w+")
+reqs = []
 for system in range(first_system, last_system + 1):
-    response = requests.get('https://esi.evetech.net/latest/universe/systems/' + str(system) + '/?datasource=tranquility&language=en')
+    req = grequests.get('https://esi.evetech.net/latest/universe/systems/' + str(system) + '/?datasource=tranquility&language=en', headers=request_headers)
+    reqs.append(req)
+print(len(reqs))
+responses = grequests.map(reqs, size=30)
+for response in responses:
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        print('Received status code {} from {}'.format(response.status_code, response.url))
+        continue
     data = response.text
     json_output = json.loads(data)
     relevant_info = {
-        'system_id' : system,
+        'system_id' : json_output['system_id'],
         'name' : json_output['name'],
         'stargates' : json_output['stargates']
     }
-    #print('----XOXOX----\n' + json_output)
-    print(relevant_info)
-
-
+    f.write(str(relevant_info) + "\n")
