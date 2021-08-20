632e110cd68be5bb501b44d750cf7b0cfdeec005
Xavier Ortiz <xavier.ortiz.ch@gmail.com>
2021-08-20 01:20:42 -0400

    fixed issue. Was not hard limit, but a key in an entry didn't exist.

--- build-graph/graphBuilder.py
+++ build-graph/graphBuilder.py
@@ -4,7 +4,7 @@ from requests.exceptions import HTTPError
 from requests_futures.sessions import FuturesSession
 
 first_system = 30000001
-last_system = 30000326
+last_system = 30045354
 system_segments=[]
 amount_of_systems = last_system - first_system + 1
 request_headers = {
@@ -15,18 +15,29 @@ request_headers = {
 
 f= open("test.txt","w+")
 reqs = []
-session = FuturesSession(executor=ThreadPoolExecutor(max_workers=10))
+session = FuturesSession(executor=ThreadPoolExecutor(max_workers=1000))
 for system in range(first_system, last_system + 1):
     req = session.get('https://esi.evetech.net/latest/universe/systems/' + str(system) + '/?datasource=tranquility&language=en', headers=request_headers)
     reqs.append(req)
 #print(reqs)
 for response in reqs:
-    data = response.result().text
+    result = response.result()
+    try:
+        result.raise_for_status()
+    except HTTPError:
+        print('Received status code {} from {}'.format(result.status_code, result.url))
+        print('Limit-Remain: {} Limit-Rest {}'.format(result.headers['x-esi-error-limit-remain'], result.headers['x-esi-error-limit-reset'] ))
+        continue
+    except requests.exceptions.RequestException as e: 
+        raise SystemExit(e)
+        continue
+    data = result.text
     json_output = json.loads(data)
-    relevant_info = {
-        'system_id' : json_output['system_id'],
-        'name' : json_output['name'],
-        'stargates' : json_output['stargates']
-    }
+    if 'stargates' in json_output:
+        relevant_info = {
+            'system_id' : json_output['system_id'],
+            'name' : json_output['name'],
+            'stargates' : json_output['stargates']
+            }
 
     f.write(str(relevant_info) + "\n")
