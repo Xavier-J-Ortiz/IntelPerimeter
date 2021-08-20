44055d09dc1ee5eb6d10bf1d95ba6af45428dc44
Xavier Ortiz <xavier.ortiz.ch@gmail.com>
2021-08-20 00:43:10 -0400

    testing threaded

--- build-graph/graphBuilder.py
+++ build-graph/graphBuilder.py
@@ -1,6 +1,7 @@
-import grequests
 import json
+from concurrent.futures import ThreadPoolExecutor
 from requests.exceptions import HTTPError
+from requests_futures.sessions import FuturesSession
 
 first_system = 30000001
 last_system = 30000326
@@ -14,22 +15,18 @@ request_headers = {
 
 f= open("test.txt","w+")
 reqs = []
+session = FuturesSession(executor=ThreadPoolExecutor(max_workers=10))
 for system in range(first_system, last_system + 1):
-    req = grequests.get('https://esi.evetech.net/latest/universe/systems/' + str(system) + '/?datasource=tranquility&language=en', headers=request_headers)
+    req = session.get('https://esi.evetech.net/latest/universe/systems/' + str(system) + '/?datasource=tranquility&language=en', headers=request_headers)
     reqs.append(req)
-print(len(reqs))
-responses = grequests.map(reqs, size=30)
-for response in responses:
-    try:
-        response.raise_for_status()
-    except HTTPError:
-        print('Received status code {} from {}'.format(response.status_code, response.url))
-        continue
-    data = response.text
+#print(reqs)
+for response in reqs:
+    data = response.result().text
     json_output = json.loads(data)
     relevant_info = {
         'system_id' : json_output['system_id'],
         'name' : json_output['name'],
         'stargates' : json_output['stargates']
     }
+
     f.write(str(relevant_info) + "\n")
