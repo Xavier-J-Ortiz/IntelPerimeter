31b674414d24c87fc1cad168d4b085b0d7767e2a
Xavier Ortiz <xavier.ortiz.ch@gmail.com>
2021-08-24 00:50:24 -0400

    found out how to use watchdog and point to a folder system to follow for change events. can now continue towards figuring out how to read the new last lines.

---
+++ watcher.py
@@ -0,0 +1,56 @@
+import pickle
+import os
+import re
+import time
+import datetime
+from watchdog.observers import Observer
+from watchdog.events import RegexMatchingEventHandler
+#from watchdog.events import PatternMatchingEventHandler
+
+from watchdog import watchmedo
+
+perimeter = pickle.load(open('build-graph/perimeter.p', 'rb'))
+
+file_path = '/home/xortiz/Documents/EVE/logs/Chatlogs/'
+#file_path = '.'
+chatlog_name = 'delve\.imperium'
+
+#def find_files(file_path, chatlog_name):
+date = datetime.datetime.utcnow()
+year = date.strftime("%Y")
+#raw_month = date.month
+month = date.strftime("%m")
+#raw_day = date.day
+day = date.strftime("%d")
+#files = os.listdir(file_path)
+regex = '^' + file_path + chatlog_name + '_' + year + month + day + '.*'
+#pattern = chatlog_name + '_' + year + month + day + '*'
+#regex = re.compile('^' + chatlog_name + '_' + year + month + day + '.*')
+#relevant = list(filter(regex.match, files))
+#return relevant
+
+if __name__ == "__main__":
+    regexes = [regex, "^" + file_path + "Fleet.*"]
+    ignore_regexes= []
+    ignore_directories = False
+    case_sensitive = True
+    my_event_handler = RegexMatchingEventHandler(regexes,ignore_regexes,ignore_directories,case_sensitive)
+    print (regexes)
+
+    def on_modified(event):
+        print(f"hey buddy, {event.src_path} has been modified")
+    
+    my_event_handler.on_modified = on_modified
+
+    go_recursively = True
+    my_observer = Observer()
+    my_observer.schedule(my_event_handler, file_path, recursive=go_recursively)
+    my_observer.schedule
+
+    my_observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        my_observer.stop()
+        my_observer.join()
