e9127dcb092480c011e3ce3b63c2460edf9b3b8e
Xavier Ortiz <xavier.ortiz.ch@gmail.com>
2021-08-24 22:30:54 -0400

    a nice MVP. It makes a sound when something approaches the perimeter!

--- watcher.py
+++ watcher.py
@@ -14,7 +14,7 @@ perimeter = pickle.load(open('build-graph/perimeter.p', 'rb'))
 
 file_path = '/home/xortiz/Documents/EVE/logs/Chatlogs/'
 #file_path = '.'
-chatlog_name = 'delve\.imperium'
+channel_names = ['delve\.imperium', 'Fleet']
 
 #def find_files(file_path, chatlog_name):
 date = datetime.datetime.utcnow()
@@ -24,26 +24,34 @@ month = date.strftime("%m")
 #raw_day = date.day
 day = date.strftime("%d")
 #files = os.listdir(file_path)
-regex = '^' + file_path + chatlog_name + '_' + year + month + day + '.*'
 #pattern = chatlog_name + '_' + year + month + day + '*'
 #regex = re.compile('^' + chatlog_name + '_' + year + month + day + '.*')
 #relevant = list(filter(regex.match, files))
 #return relevant
 
 if __name__ == "__main__":
-    regexes = [regex, "^" + file_path + "Fleet.*"]
+    regexes = []
+    for channel in channel_names:
+        regexes.append('^' + file_path + channel + '_' + year + month + day + '.*')
+    print (regexes)
     ignore_regexes= []
     ignore_directories = False
     case_sensitive = True
     my_event_handler = RegexMatchingEventHandler(regexes,ignore_regexes,ignore_directories,case_sensitive)
-    print (regexes)
 
     def on_modified(event):
         print(f"hey buddy, \'{event.src_path}\' has been modified")
         with open(event.src_path, 'r', encoding='utf-16') as changed_log:
             last_line = changed_log.readlines()[-1]
-            #alarm = vlc.MediaPlayer('alarms/alarm2.mp3')
-            #alarm.play()
+            analyze = last_line.replace('\n',' ').split(" ")
+            analyze_lowercase = [entry.lower() for entry in analyze]
+            print(analyze_lowercase)
+            for system in perimeter:
+                #print(system)
+                if system.lower() in analyze_lowercase:
+                    print(f'DANGER: {system} : {perimeter[system]} jumps' ) 
+                    alarm = vlc.MediaPlayer('alarms/alarm2.mp3')
+                    alarm.play()
             print(last_line)
 
     
